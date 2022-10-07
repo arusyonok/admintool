c67f1f2a0b8c0df93bc50a83ba78e50eaa89bb93
Arus Sahakyan <arus.sahakyan@gmail.com>
2022-10-07 15:10:37 +0300

    add users and spending groups

--- finances/admin.py
+++ finances/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 from .models import *
 
@@ -9,10 +10,23 @@ class TransactionAdmin(admin.ModelAdmin):
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'type')
 
+
 class SubCategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'parent')
 
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name')
+
+
+class SpendingGroupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'group_members')
+
+    def group_members(self, obj):
+        return ", ".join([f"{user}" for user in obj.users.all()])
+
+admin.site.register(User, UserAdmin)
+admin.site.register(SpendingGroup, SpendingGroupAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(SubCategory, SubCategoryAdmin)
 admin.site.register(Transaction, TransactionAdmin)
-
--- finances/models.py
+++ finances/models.py
@@ -20,6 +20,31 @@ RECORD_TYPES = (
     (2, "Transfer")
 )
 
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        if self.first_name or self.last_name:
+            full_name = f"{self.first_name} {self.last_name} ({self.username})"
+            no_first_name = f"{self.last_name} ({self.username})"
+            display_name = full_name if self.first_name else no_first_name
+        else:
+            display_name = self.username
+
+        return display_name
+
+
+class SpendingGroup(models.Model):
+    title = models.CharField(max_length=50)
+    users = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.title
+
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
     type = models.IntegerField(choices=RECORD_TYPES, default=0) # TODO: fix this default
