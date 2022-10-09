e3b6db525d6cb2a22c09141611114697a663b357
Arus Sahakyan <arus.sahakyan@gmail.com>
2022-10-10 01:21:53 +0300

    add a view to view expense list

--- finances/views.py
+++ finances/views.py
@@ -1,8 +1,15 @@
 from django.views.generic import TemplateView
+from catalog.common import RecordTypes
+from .models import PersonalRecord
 
 
 class PersonalExpenseView(TemplateView):
     template_name = 'finances/personal_expenses.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['expenses'] = PersonalRecord.objects.filter(type=RecordTypes.EXPENSE)
+        return context
 
 
 class PersonalIncomeView(TemplateView):
