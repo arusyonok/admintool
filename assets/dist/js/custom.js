872f1c9a9b22eef451c9c1baa81388fc993822d9
Arus Sahakyan <arus.sahakyan@gmail.com>
2022-12-01 16:54:40 +0200

    create js block for particular pages

--- assets/dist/js/custom.js
+++ assets/dist/js/custom.js
@@ -12,27 +12,4 @@ $(document).ready(function() {
         var form = $(this).children().find("form");
         form.attr("action", url)
     });
-
-
-    //-------------
-    //- BALANCE HORIZONTAL BAR CHART -
-    //-------------
-
-    var barChartCanvas = $('#barChart')
-    var barChartDataset = JSON.parse(barChartCanvas.attr("data-datasets"))
-    var barChartOptions = {
-        legend: {
-            display: false,
-        },
-        responsive: true,
-        maintainAspectRatio: false,
-        datasetFill: false,
-    }
-    var barChartConfig = {
-      type: 'horizontalBar',
-      data: barChartDataset,
-      options: barChartOptions
-    };
-
-    var chart = new Chart(barChartCanvas, barChartConfig)
 });
