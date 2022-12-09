0040b63b723dda8168aaa23bf5ef469b94932fda
Arus Sahakyan <arus.sahakyan@gmail.com>
2022-12-09 10:53:33 +0200

    move record_type field to the abstract model

--- finances/models.py
+++ finances/models.py
@@ -10,6 +10,7 @@ class AbstractRecord(models.Model):
     amount = models.DecimalField(decimal_places=2, max_digits=10)
     date = models.DateTimeField(auto_now_add=True)
     sub_category = models.ForeignKey(SubCategory, on_delete=models.SET_NULL, null=True)
+    record_type = models.IntegerField(choices=RecordTypes.CHOICES)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -25,13 +26,17 @@ class AbstractRecord(models.Model):
 class PersonalWalletRecord(AbstractRecord):
     personal_wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
     user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    record_type = models.IntegerField(choices=RecordTypes.CHOICES)
 
 
 class GroupWalletRecord(AbstractRecord):
     group_wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
     paid_by = models.ForeignKey(Profile, related_name="paid_by_me_set", on_delete=models.SET_NULL, null=True)
     paid_for_users = models.ManyToManyField(Profile, related_name="paid_for_me_set")
+
+    def save(self, *args, **kwargs):
+        # group wallet record should not be anything else
+        self.record_type = RecordTypes.EXPENSE
+        super(GroupWalletRecord, self).save(*args, **kwargs)
 
 
 class Balance(models.Model):
